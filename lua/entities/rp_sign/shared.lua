d13234a2e2c5e608ccb778d204a74a25a78f1e44
Doug Young <fluid781@gmail.com>
2014-10-29 23:04:28 -0400

    Create shared.lua

---
+++ lua/entities/rp_sign/shared.lua
@@ -0,0 +1,15 @@
+ENT.Base = "base_gmodentity"
+ENT.Type = "anim"
+ENT.Spawnable = true
+
+ENT.PrintName		= "Sign"
+ENT.Author			= "DougRiss"
+ENT.Contact			= "N/A"
+ENT.Purpose			= "Save info in a Sign for other players to read!"
+ENT.Instructions	= "Hit E to edit the sign"
+
+function ENT:SetupDataTables() --I used these to initialize the ents with sign name and value attributes at spawn, basically.
+	self:NetworkVar( "String", 0, "SignName" )
+	self:NetworkVar( "String", 1, "SignText" )
+	self:NetworkVar( "String", 3, "SignOwner")
+end 
