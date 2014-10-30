db318b3f7e23534e7f39202a9d7f9534258d6250
Doug Young <fluid781@gmail.com>
2014-10-29 23:01:34 -0400

    Create init.lua
    after 4 iterations and plenty of trying the fuck out of getting it to work, it's finally up.
    
    This is the server file for the RP Signs entities.

---
+++ lua/entities/rp_sign/init.lua
@@ -0,0 +1,64 @@
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("init.lua")
+include("shared.lua")
+
+util.AddNetworkString("nwrpsign") -- communicate between client and server about the signs
+
+--[[-----------------------------------
+Here's the Signs' table and things
+touch it.
+--]]-----------------------------------
+local signs = {}
+	
+net.Receive("nwrpsign", -- Not sure if this is the best way to do it, but this is all I knew about talking with the server.
+	function (len)
+		local theTable = net.ReadTable()
+		placeSignInTable(theTable.name, theTable, theTable.owner)
+	end)
+
+function placeSignInTable(id, theTable, owner)
+	signs[id] = theTable
+end
+--[[-----------------------------------
+		Average Ent Shit
+--]]-----------------------------------
+
+function ENT:SetupDataTables() --I used these to initialize the ents with sign name and value attributes at spawn, basically.
+	self:NetworkVar( "String", 0, "SignName" )
+	self:NetworkVar( "String", 1, "SignText" )
+	self:NetworkVar( "String", 3, "SignOwner")
+end 
+
+function ENT:Initialize()
+	self:SetModel("models/props_lab/clipboard.mdl")
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetSignName(self:GetCreationID()) --sets the sign's name to it's creationID
+	self:SetSignText("")
+	self:SetSignOwner("")
+	local newSign = {} 
+	newSign.name = self:GetSignName()
+	newSign.text = self:GetSignText()
+	newSign.owner = self:GetSignOwner()
+	signs[self:GetSignName()] = newSign --store the newly created sign in the table, right?
+end
+
+function ENT:OnRemove()
+	signs[self:GetSignName()] = nil -- remove it when deleted (gotta free up dem bits n bytes)
+end
+
+function ENT:Use( act, call )
+	local me = self:GetSignName() -- on use, get the name of the entity being used
+	local theTable = signs[me]			-- store the table entry associated with whatever 'me' turns out to be
+	theTable.activator = act
+	if act.IsPlayer() then  	-- if the activating entity is the player...
+		net.Start("nwrpsign") 	-- start the network string
+			net.WriteTable(theTable) 	-- write the table with our entity's information
+		net.Send(act) 			-- then send to the player who used it.
+	end
+end
+
+function ENT:Think()
+	--I am sign I am think hurdur--
+end
