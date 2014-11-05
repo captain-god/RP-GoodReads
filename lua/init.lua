18fe277f611153a4dcc7d7bdb209d13b612eb00c
Doug Young <fluid781@gmail.com>
2014-11-05 09:37:25 -0500

    Delete init.lua

--- lua/init.lua
+++
@@ -1,5 +0,0 @@
---[[import all files for the mod]]--
-IncludeCSLuaFile('entities/rp_sign/init.lua')
-IncludeCSLuaFile('entities/rp_book/init.lua')
---IncludeCSLuaFile('includes/derma/init.lua') --file doesn't exist yet.
---[[end import]]--
