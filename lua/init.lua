b40f16b466defdfed66b90f60f6d12b9ffe8c4c2
Doug Young <fluid781@gmail.com>
2014-11-05 09:28:22 -0500

    Create init.lua

---
+++ lua/init.lua
@@ -0,0 +1,5 @@
+--[[import all files for the mod]]--
+--[[import all files for the mod]]--
+--[[import all files for the mod]]--
+--[[import all files for the mod]]--
+--[[import all files for the mod]]--
