201f745a1fdad6566947f172c0b2b9b8e80c3776
Doug Young <fluid781@gmail.com>
2014-11-05 09:30:27 -0500

    Update init.lua

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,5 @@
 --[[import all files for the mod]]--
---[[import all files for the mod]]--
---[[import all files for the mod]]--
---[[import all files for the mod]]--
---[[import all files for the mod]]--
+IncludeCSLuaFile('entities/rp_sign/init.lua')
+IncludeCSLuaFile('entities/rp_book/init.lua')
+--IncludeCSLuaFile('includes/derma/init.lua') --file doesn't exist yet.
+--[[end import]]--
