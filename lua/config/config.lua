0e2cbe4ebd7faf667e584e450cc149241416ff99
Doug Young <fluid781@gmail.com>
2014-11-06 09:34:16 -0500

    Update config.lua

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -1,22 +1,21 @@
---[[Server Configuration files:      ]]--
---[[As of 11/5/14, this does nothing.]]--
---[[---------------------------------]]--
---[[Typical install, 8-person server ]]--
---[[ready.                           ]]--
---[[---------------------------------]]--
+--[[   Server Configuration files:   ]]--
 
 --Server Admins do not subscribe to server limits.
-SV_ADMIN_IGNORE = false 
--- Maximum amount of signs a player can have at once.
-SV_MAX_SIGNS = 5 
+ADMIN_IGNORE = false 
+--Maximum amount of signs a player can have at once.
+MAX_SIGNS = 5 
 --maximum number of characters allowed in a sign, 0 means no limit
-SV_MAX_SIGN_CHARACTERS = 255
+MAX_SIGN_CHARACTERS = 255
 --maximum number of characters allowed in a book, 0 means no limit
-SV_MAX_BOOK_CHARACTERS = 255 
+MAX_BOOK_CHARACTERS = 255 
 --maximum number of chraracters allowed in a sticky-note; must be > 0 & < 255 (sticky notes are small)
-SV_MAX_STICKY_CHARACTERS = 60 
---maximum text message characters
-SV_MAX_TXTMSG_CHARACTERS = 164
---To Finish/Refine
-
---[[ END OF FILE ]]--
+MAX_STICKY_CHARACTERS = 60 
+--minimum number of characters allowed; must be > 0
+MIN_CHARACTERS = 0 
+--players can use signs
+ALLOW_SIGNS = true
+--players can use books
+ALLOW_BOOKS = true
+--players can use sticky notes
+ALLOW_NOTES = true
+--TODO: Finish
