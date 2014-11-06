6d800e77a45c8a3402e8a31bca6cf32fa0384f73
Doug Young <fluid781@gmail.com>
2014-11-06 14:32:08 -0500

    Create usgstat.lua

---
+++ lua/config/usgstat.lua
@@ -0,0 +1,4 @@
+--[[
+The intent of this file is to create a usage report highlighting "fun facts" about the mod and its usage.
+Presently, it does not do that. This will be for a later version, preferably after release.
+]]--
