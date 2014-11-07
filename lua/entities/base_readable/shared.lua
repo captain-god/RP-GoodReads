3a4a3aa71a295e821bb124de805017c130a1ebea
Doug Young <fluid781@gmail.com>
2014-11-07 11:10:47 -0500

    Delete shared.lua

--- lua/entities/base_readable/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Base = "base_gmodentity"
-ENT.Type = "anim"
-ENT.Spawnable = true
-
-ENT.PrintName		= "readable"
-ENT.Author			= "DougRiss"
-ENT.Contact			= "himself@dougriss.com"
-ENT.Purpose			= "Base for all readables in RPGoodReads"
-ENT.Instructions	= "Derive other ents from me!"
